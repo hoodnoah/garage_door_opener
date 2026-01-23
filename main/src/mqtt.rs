56f0e8d6901bf27b96ccfab98549cf4d1f01ce64
Noah Hood <hood.noah@icloud.com>
2026-01-22 20:08:22 -0500

    add command reception to mqtt

--- main/src/mqtt.rs
+++ main/src/mqtt.rs
@@ -1,7 +1,7 @@
-use std::time::Duration;
+use std::{sync::{Arc, Mutex}, time::Duration};
 
 use esp_idf_svc::{
-    mqtt::client::{EspMqttClient, MqttClientConfiguration, QoS},
+    mqtt::client::{EspMqttClient, EventPayload, MqttClientConfiguration, QoS},
     sys::EspError,
 };
 use lib::state_machine::GDState;
@@ -9,9 +9,26 @@ use lib::state_machine::GDState;
 const STATE_TOPIC: &str = "basement_garage/state";
 const STATUS_TOPIC: &str = "basement_garage/status";
 const ERROR_TOPIC: &str = "basement_garage/error";
+const COMMAND_TOPIC: &str = "basement_garage/command";
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum GarageCommand {
+    Open,
+    Close,
+}
+
+impl GarageCommand {
+    pub fn from_bytes(bytes: &[u8]) -> Option<Self> {
+        match bytes {
+            b"open" => Some(GarageCommand::Open),
+            b"close" => Some(GarageCommand::Close),
+            _ => None,
+        }
+}
 
 pub struct GDMQTT<'a> {
     client: EspMqttClient<'a>,
+    last_command: Arc<Mutex<Option<GarageCommand>>>,
 }
 
 impl<'a> GDMQTT<'a> {
@@ -28,9 +45,30 @@ impl<'a> GDMQTT<'a> {
             ..Default::default()
         };
 
-        let (client, _) = EspMqttClient::new(mqtt_endpoint, &config)?;
+        let last_command = Arc::new(Mutex::new(None));
+        let last_command_clone = last_command.clone();
 
-        Ok(Self { client })
+        let (mut client, mut connection) = EspMqttClient::new(mqtt_endpoint, &config)?;
+
+        // Subscribe to the command topic
+        client.subscribe(COMMAND_TOPIC, QoS::AtLeastOnce)?;
+
+        // Spawn thread to handle incoming messages
+        std::thread::spawn(move || {
+            while let Ok(event) = connection.next() {
+                if let EventPayload::Received {data, ..} = event.payload() {
+                    if let Some(cmd) = GarageCommand::from_bytes(data) {
+                        *last_command_clone.lock().unwrap() = Some(cmd);
+                    }
+                }
+            }
+        });
+
+        Ok(Self { client, last_command })
+    }
+
+    pub fn take_command(&mut self) -> Option<GarageCommand>  {
+        self.last_command.lock().unwrap().take()
     }
 
     pub fn publish_state(&mut self, state: GDState) -> Result<u32, EspError> {
@@ -51,4 +89,5 @@ impl<'a> GDMQTT<'a> {
         self.client
             .publish(ERROR_TOPIC, QoS::AtLeastOnce, false, error_msg.as_bytes())
     }
+}
 }
